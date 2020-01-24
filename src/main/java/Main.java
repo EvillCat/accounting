50477310d9d5767534b3535f8afbc9a70a1ca434
Зарицкий Иван <ivan.zaritsky@gmail.com>
2020-01-25 02:43:46 +0700

    - Кол-во потоков перенесено обратно в "магическое" число, т.к. реализация не завязана на их изменение.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,11 @@ import java.util.concurrent.*;
 
 public class Main {
 
-    private static final int THREAD_COUNT = 2;
     private static final int TRANSACTIONS_COUNT = 30;
     private static final int MONEY = 10000;
 
     public static void main(String[] args) {
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(THREAD_COUNT);
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
         TransactionService transactionService = new TransactionService(executorService, TRANSACTIONS_COUNT);
 
         Account bobAccount = new Account(MONEY);
