6b2abbee46fb438e16a8cc4927c80fa753d40383
Зарицкий Иван <ivan.zaritsky@gmail.com>
2020-01-25 10:52:59 +0700

    - поправлены значения констант на те, что в тз.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.concurrent.*;
 
 public class Main {
 
-    private static final int TRANSACTIONS_COUNT = 600000;
+    private static final int TRANSACTIONS_COUNT = 30;
     private static final int MONEY = 10000;
 
     public static void main(String[] args) {
